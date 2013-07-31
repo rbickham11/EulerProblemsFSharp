46e1319604dcbf618d58d055d87680383620935a
rbickham11 <rcbickha@svsu.edu>
2013-07-31 10:05:14 -0400

    Problems 1-2

---
+++ EulerProblemsFSharp/Euler1_10.fs
@@ -0,0 +1,16 @@
+﻿module EulerProblemsFSharp.Euler1_10
+
+open System
+
+type Problems() =
+    member x.problem1 =  //Find the sum of all the multiples of 3 or 5 below 1000.
+        printf "Problem 1: "
+        [1..999]
+        |> Seq.sumBy(fun n -> (if (n % 3 = 0) || (n % 5 = 0) then n else 0))
+
+    member x.problem2 =  //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+        printf "Problem 2: "
+        Seq.unfold(fun (this, next) -> Some(this, (next, this + next))) (0, 1)
+        |> Seq.takeWhile(fun x -> x <= 4000000)
+        |> Seq.filter(fun x -> x % 2 = 0)
+        |> Seq.sum
---
+++ EulerProblemsFSharp/EulerMain.fs
@@ -0,0 +1,15 @@
+﻿module EulerProblemsFSharp.EulerMain
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let stopWatch = new System.Diagnostics.Stopwatch()
+    let eu1 = new EulerProblemsFSharp.Euler1_10.Problems()
+
+    stopWatch.Restart()
+    printfn "%A (%dms)" eu1.problem1 stopWatch.ElapsedMilliseconds
+    stopWatch.Restart()
+    printfn "%A (%dms)" eu1.problem2 stopWatch.ElapsedMilliseconds
+    Console.ReadKey() |> ignore
+    0
